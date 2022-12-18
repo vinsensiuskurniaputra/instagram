fafe92a4db08720a7cf89a89d8283c8c2a11bcd3
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2022-12-18 18:50:22 +0700

    style the story section

--- components/Story.tsx
+++ components/Story.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Story = ({username, img}) => {
   return (
     <div>
-        <img src={img} alt={username} />
-        <p>{username}</p>
+      <img src={img} alt={username} className='rounded-full h-14 p-[1.5px] border-2 border-red-500 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out' />
+      <p className='text-xs w-14 truncate'>{username}</p>
     </div>
-  )
+  );
 }
 
 export default Story
