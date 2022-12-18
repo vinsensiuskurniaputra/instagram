f8e4a5d56339b3178a22da517d8ce1b8f615d5a0
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2022-12-18 19:37:07 +0700

    create button post

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,5 @@
 import { DotsHorizontalIcon } from "@heroicons/react/solid";
+import { HeartIcon, ChatIcon, BookmarkIcon } from "@heroicons/react/outline";
 import React from "react";
 
 const Post = ({ username, userImg, img, caption }) => {
@@ -12,10 +13,18 @@ const Post = ({ username, userImg, img, caption }) => {
           className='rounded-full h-12 object-cover p-1 border-2 border-gray-100'
         />
         <h1 className='font-bold grow mx-2'>{username}</h1>
-        <DotsHorizontalIcon className='h-5' />
+        <DotsHorizontalIcon className='h-5 btn' />
       </div>
       {/* Post Image */}
       <img src={img} alt="Post" className="object-cover w-full "/>
+      {/* Post Button */}
+        <div className="flex px-4 pt-4 justify-between">
+            <div className="flex space-x-4">
+            <HeartIcon className="btn"/>
+            <ChatIcon className="btn"/>
+            </div>
+            <BookmarkIcon className="btn"/>
+        </div>
     </div>
   );
 };
