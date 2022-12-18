ce402606d5d19fa4dab90e8c17cae11403327c98
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2022-12-18 19:24:29 +0700

    make style header post

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
+import React from "react";
 
-const Post = ({username, userImg, img, caption}) => {
+const Post = ({ username, userImg, img, caption }) => {
   return (
-    <div>
-        <h1>{username}</h1>
+    <div className="bg-white rounded-md my-7 border">
+      {/* Post Header */}
+      <div className='flex items-center p-5'>
+        <img
+          src={userImg}
+          alt={username}
+          className='rounded-full h-12 object-cover p-1 border-2 border-gray-100'
+        />
+        <h1 className='font-bold grow mx-2'>{username}</h1>
+        <DotsHorizontalIcon className='h-5' />
+      </div>
+      {/* Post Image */}
+      <img src={img} alt="Post" className="object-cover w-full "/>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
